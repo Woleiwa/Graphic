029b218523378aa732eb55d6bb0e406902f91053
njuzyy <zhongyyoo0@163.com>
2022-10-20 14:59:08 +0800

    october

---
+++ CG_demo/cg_algorithms.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# 本文件只允许依赖math库
+import math
+
+
+def draw_line(p_list, algorithm):
+    """绘制线段
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1]]) 线段的起点和终点坐标
+    :param algorithm: (string) 绘制使用的算法，包括'DDA'和'Bresenham'，此处的'Naive'仅作为示例，测试时不会出现
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1], [x_2, y_2], ...]) 绘制结果的像素点坐标列表
+    """
+    x0, y0 = p_list[0]
+    x1, y1 = p_list[1]
+    result = []
+    if algorithm == 'Naive':
+        if x0 == x1:
+            for y in range(y0, y1 + 1):
+                result.append((x0, y))
+        else:
+            if x0 > x1:
+                x0, y0, x1, y1 = x1, y1, x0, y0
+            k = (y1 - y0) / (x1 - x0)
+            for x in range(x0, x1 + 1):
+                result.append((x, int(y0 + k * (x - x0))))
+    elif algorithm == 'DDA':
+        dx = x1 - x0
+        dy = y1 - y0
+        abs_dx = abs(dx)
+        abs_dy = abs(dy)
+        if abs_dx >= abs_dy:
+            step = abs_dx
+        else:
+            step = abs_dy
+        increx = float(dx)/step
+        increy = float(dy)/step
+        cur_x = float(x0)
+        cur_y = float(y0)
+        while (cur_x != float(x1)) | (cur_y != float(y1)):
+            result.append((int(cur_x), int(cur_y)))
+            cur_x += increx
+            cur_y += increy
+        result.append((x1, y1))
+    elif algorithm == 'Bresenham':
+        dx = abs(x1-x0)
+        if x0 < x1:
+            sx = 1
+        else:
+            sx = -1
+        dy = abs(y1-y0)
+        if y0 < y1:
+            sy = 1
+        else:
+            sy = -1
+        if dx > dy:
+            err = dx
+        else:
+            err = -dy
+        while (x0 != x1) | (y0 != y1):
+            result.append((x0, y0))
+            e2 = err
+            if e2 > -2*dx:
+                err -= 2*dy
+                x0 += sx
+            elif e2 < 2*dy:
+                err += 2*dx
+                y0 += sy
+        result.append((x1, y1))
+    return result
+
+
+def draw_polygon(p_list, algorithm):
+    """绘制多边形
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1], [x2, y2], ...]) 多边形的顶点坐标列表
+    :param algorithm: (string) 绘制使用的算法，包括'DDA'和'Bresenham'
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1], [x_2, y_2], ...]) 绘制结果的像素点坐标列表
+    """
+    result = []
+    for i in range(len(p_list)):
+        line = draw_line([p_list[i - 1], p_list[i]], algorithm)
+        result += line
+    return result
+
+
+def draw_ellipse(p_list):
+    """绘制椭圆（采用中点圆生成算法）
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1]]) 椭圆的矩形包围框左上角和右下角顶点坐标
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1], [x_2, y_2], ...]) 绘制结果的像素点坐标列表
+    """
+    x0 = int(p_list[0][0])
+    y0 = int(p_list[0][1])
+    x1 = int(p_list[1][0])
+    y1 = int(p_list[1][1])
+    if abs(x1 - x0) % 2 == 1:
+        a = (abs(x1 - x0) + 1) / 2
+    else:
+        a = abs(x1 - x0) / 2
+    if abs(y1 - y0) % 2 == 1:
+        b = (abs(y0 - y1) + 1) / 2
+    else:
+        b = abs(y0 - y1) / 2
+    cur_x = 0
+    cur_y = int(b)
+    pl = pow(b, 2) - pow(a, 2) * b + pow(a, 2) / 4
+    judge = pow(b, 2)*cur_x - pow(a, 2)*cur_y
+    res = []
+    while judge < 0:
+        res.append((int(cur_x), int(cur_y)))
+        if pl < 0:
+            next_x = cur_x + 1
+            next_y = cur_y
+            pl = pl + 2 * pow(b, 2) * next_x + pow(b, 2)
+        else:
+            next_x = cur_x + 1
+            next_y = cur_y - 1
+            pl = pl + 2 * pow(b, 2) * next_x - 2 * pow(a, 2) * next_y + pow(b, 2)
+        cur_x = next_x
+        cur_y = next_y
+        judge = pow(b, 2)*cur_x - pow(a, 2)*cur_y
+
+    pl = pow(b, 2) * (cur_x + 1/2) * (cur_x + 1/2) + pow(a, 2) * (cur_y - 1) * (cur_y - 1) - pow(b, 2) * pow(a, 2)
+
+    while cur_y > 0:
+        res.append((int(cur_x), int(cur_y)))
+        if pl >= 0:
+            next_x = cur_x
+            next_y = cur_y - 1
+            pl = pl - 2 * pow(a, 2) * next_y + pow(a, 2)
+        else:
+            next_x = cur_x + 1
+            next_y = cur_y - 1
+            pl = pl + 2 * pow(b, 2) * next_x - 2 * pow(a, 2) * next_y + pow(a, 2)
+        cur_x = next_x
+        cur_y = next_y
+    res.append((cur_x, cur_y))
+    final_res = []
+    for point in res:
+        x = int(point[0])
+        y = int(point[1])
+        final_res.append((x, y))
+        final_res.append((x, -y))
+        final_res.append((-x, y))
+        final_res.append((-x, -y))
+    return translate(final_res, int((x0 + x1)/2), int((y0 + y1)/2))
+
+
+def draw_curve(p_list, algorithm):
+    """绘制曲线
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1], [x2, y2], ...]) 曲线的控制点坐标列表
+    :param algorithm: (string) 绘制使用的算法，包括'Bezier'和'B-spline'（三次均匀B样条曲线，曲线不必经过首末控制点）
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1], [x_2, y_2], ...]) 绘制结果的像素点坐标列表
+    """
+    pass
+
+
+def translate(p_list, dx, dy):
+    """平移变换
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1], [x2, y2], ...]) 图元参数
+    :param dx: (int) 水平方向平移量
+    :param dy: (int) 垂直方向平移量
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1], [x_2, y_2], ...]) 变换后的图元参数
+    """
+    res = []
+    for point in p_list:
+        x = int(point[0]) + dx
+        y = int(point[1]) + dy
+        res.append((x, y))
+    return res
+
+
+def rotate(p_list, x, y, r):
+    """旋转变换（除椭圆外）
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1], [x2, y2], ...]) 图元参数
+    :param x: (int) 旋转中心x坐标
+    :param y: (int) 旋转中心y坐标
+    :param r: (int) 顺时针旋转角度（°）
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1], [x_2, y_2], ...]) 变换后的图元参数
+    """
+    cos = math.cos(r/180 * math.pi)
+    sin = math.cos(r/180 * math.pi)
+    res = []
+    for point in p_list:
+        cur_x = int(point[0])
+        cur_y = int(point[1])
+        next_x = x + (cur_x - x) * cos - (cur_y - y) * sin
+        next_y = y + (cur_x - x) * sin + (cur_y - y) * cos
+        res.append((int(next_x), int(next_y)))
+    return res
+
+
+def scale(p_list, x, y, s):
+    """缩放变换
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1], [x2, y2], ...]) 图元参数
+    :param x: (int) 缩放中心x坐标
+    :param y: (int) 缩放中心y坐标
+    :param s: (float) 缩放倍数
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1], [x_2, y_2], ...]) 变换后的图元参数
+    """
+    pass
+
+
+def clip(p_list, x_min, y_min, x_max, y_max, algorithm):
+    """线段裁剪
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1]]) 线段的起点和终点坐标
+    :param x_min: 裁剪窗口左上角x坐标
+    :param y_min: 裁剪窗口左上角y坐标
+    :param x_max: 裁剪窗口右下角x坐标
+    :param y_max: 裁剪窗口右下角y坐标
+    :param algorithm: (string) 使用的裁剪算法，包括'Cohen-Sutherland'和'Liang-Barsky'
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1]]) 裁剪后线段的起点和终点坐标
+    """
+    pass
---
+++ CG_demo/cg_cli.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import sys
+import os
+import cg_algorithms as alg
+import numpy as np
+from PIL import Image
+
+
+if __name__ == '__main__':
+    #input_file = sys.argv[1]
+    #output_dir = sys.argv[2]
+    input_file = 'test.txt'
+    output_dir = 'res'
+    os.makedirs(output_dir, exist_ok=True)
+
+    item_dict = {}
+    point_dict = {}
+    pen_color = np.zeros(3, np.uint8)
+    width = 0
+    height = 0
+
+    with open(input_file, 'r') as fp:
+        line = fp.readline()
+        while line:
+            line = line.strip().split(' ')
+            if line[0] == 'resetCanvas':
+                width = int(line[1])
+                height = int(line[2])
+                item_dict = {}
+                point_dict = {}
+            elif line[0] == 'saveCanvas':
+                save_name = line[1]
+                canvas = np.zeros([height, width, 3], np.uint8)
+                canvas.fill(255)
+                for pixels, color in point_dict.values():
+                    for x, y in pixels:
+                        canvas[height - 1 - y, x] = color
+                Image.fromarray(canvas).save(os.path.join(output_dir, save_name + '.bmp'), 'bmp')
+
+            elif line[0] == 'setColor':
+                pen_color[0] = int(line[1])
+                pen_color[1] = int(line[2])
+                pen_color[2] = int(line[3])
+            elif line[0] == 'drawLine':
+                item_id = line[1]
+                x0 = int(line[2])
+                y0 = int(line[3])
+                x1 = int(line[4])
+                y1 = int(line[5])
+                algorithm = line[6]
+                item_dict[item_id] = ['line', [[x0, y0], [x1, y1]], algorithm, np.array(pen_color)]
+                p_list = item_dict[item_id][1]
+                color = item_dict[item_id][3]
+                point_dict[item_id] = [pixels, color]
+                pixels = alg.draw_line(p_list, algorithm)
+                point_dict[item_id] = [pixels, color]
+            elif line[0] == 'drawPolygon':
+                item_id = line[1]
+                line_len = len(line)
+                i = 2
+                pointlist = []
+                while i < line_len - 1:
+                    x = int(line[i])
+                    y = int(line[i + 1])
+                    i += 2
+                    pointlist.append([x, y])
+                algorithm = line[line_len - 1]
+                item_dict[item_id] = ['polygon', pointlist, algorithm, np.array(pen_color)]
+                p_list = item_dict[item_id][1]
+                color = item_dict[item_id][3]
+                point_dict[item_id] = [pixels, color]
+                pixels = alg.draw_polygon(p_list, algorithm)
+                point_dict[item_id] = [pixels, color]
+            elif line[0] == 'drawEllipse':
+                item_id = line[1]
+                x0 = int(line[2])
+                y0 = int(line[3])
+                x1 = int(line[4])
+                y1 = int(line[5])
+                algorithm = ''
+                item_dict[item_id] = ['ellipse', [[x0, y0], [x1, y1]], algorithm, np.array(pen_color)]
+                p_list = item_dict[item_id][1]
+                color = item_dict[item_id][3]
+                point_dict[item_id] = [pixels, color]
+                pixels = alg.draw_ellipse(p_list)
+                point_dict[item_id] = [pixels, color]
+            elif line[0] == 'rotate':
+                item_id = line[1]
+                pixels = point_dict[item_id][0]
+                x = int(line[2])
+                y = int(line[3])
+                a = int(line[4])
+                res = alg.rotate(pixels, x, y, a)
+                point_dict[item_id][0] = res
+            elif line[0] == 'translate':
+                item_id = line[1]
+                pixels = point_dict[item_id][0]
+                x = int(line[2])
+                y = int(line[3])
+                res = alg.translate(pixels, x, y)
+                point_dict[item_id][0] = res
+            ...
+
+            line = fp.readline()
+
